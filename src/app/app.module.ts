243a70d111bbf957a813c18c7270f5e0d59cd501
pablo12b <148280248+pablo12b@users.noreply.github.com>
2023-11-23 08:41:13 -0500

    Se sube pwa
    funciona pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { FIREBASE_APP_NAME, FIREBASE_OPTIONS } from '@angular/fire/compat';
 import { RecetasService } from './recetas.service';
 import { RecetasFirebaseService } from './services/recetas-firebase.service';
 import { ViewRecetaComponent } from './pages/view-receta/view-receta.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -35,6 +36,13 @@ import { ViewRecetaComponent } from './pages/view-receta/view-receta.component';
     FormsModule,
     ReactiveFormsModule,
     provideFirebaseApp(() => initializeApp({apiKey: "AIzaSyAhkyyWgtjztkzf_zskV-0aMipjuIzeE5s", authDomain: "practicaclase-460cf.firebaseapp.com", projectId: "practicaclase-460cf", storageBucket: "practicaclase-460cf.appspot.com", messagingSenderId: "1030387281024", appId: "1:1030387281024:web:3380770ac9f952be998a65", measurementId: "G-8HGVVBGEF0"})),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+    provideFirebaseApp(() => initializeApp({"projectId":"practicaclase-460cf","appId":"1:1030387281024:web:3380770ac9f952be998a65","storageBucket":"practicaclase-460cf.appspot.com","apiKey":"AIzaSyAhkyyWgtjztkzf_zskV-0aMipjuIzeE5s","authDomain":"practicaclase-460cf.firebaseapp.com","messagingSenderId":"1030387281024","measurementId":"G-8HGVVBGEF0"})),
   ],
   providers: [{ provide: FIREBASE_OPTIONS, useValue: environment.firebaseConfig }, RecetasFirebaseService],
   bootstrap: [AppComponent]
