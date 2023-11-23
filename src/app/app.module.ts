9b9bbaf5000001371c920f5a6a1c7c51734c590a
pablo12b <148280248+pablo12b@users.noreply.github.com>
2023-11-23 08:01:21 -0500

    Se sube el programa
    Se sube la practica recetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"practicaclase-460cf","appId":"1:1030387281024:web:8083bbe935b9f379998a65","storageBucket":"practicaclase-460cf.appspot.com","apiKey":"AIzaSyAhkyyWgtjztkzf_zskV-0aMipjuIzeE5s","authDomain":"practicaclase-460cf.firebaseapp.com","messagingSenderId":"1030387281024","measurementId":"G-YCQHP830YK"})),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
